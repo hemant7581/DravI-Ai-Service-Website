c1ffa2d4fd29c2cefbb36028c4c1e17a30f76e4c
Hemant <Srivastavhemant17@gmail.com>
2023-06-05 23:14:37 +0530

    added files

---
+++ index.js
@@ -0,0 +1,16 @@
+// Smooth scroll effect
+const links = document.querySelectorAll('nav a');
+for (const link of links) {
+  link.addEventListener('click', clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute('href');
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop - 100,
+    behavior: "smooth"
+  });
+}
